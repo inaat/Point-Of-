383d7914114ccb3ed95cf4c5d764c338ad30674d
inaat <inayatullahkks@gmail.com>
2019-08-24 21:50:26 -0700

    inm

--- POS/Products/views.py
+++ POS/Products/views.py
@@ -6,14 +6,30 @@ from django.http import JsonResponse
 from django.http import HttpRequest
 import json
 # Create your 
-
-
+import os, sys
+def handle_uploaded_file(f):
+    destination = open('name.txt', 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
 def Product_Info(request):
     cursor = connections["form1_db"].cursor()
     inventorydb = connections["inventory"].cursor()
     if request.method == "POST":
         print(request.FILES['image'])
-
+        img = request.FILES['image']
+        user_folder = 'static/profile/'
+        img_extension = os.path.splitext(img.name)[1]
+        if not os.path.exists(user_folder):
+            os.mkdir(user_folder)
+        io='avatar' 
+        img_save_path="{},{},{}".format(user_folder,io,img_extension)
+        with open(img_save_path, 'wb+') as f:
+            for chunk in img.chunks():
+                f.write(chunk)    
+        # handle_uploaded_file(request.FILES['image'])
+        
+            
     if request.method == "POST"  and request.is_ajax():
         
         ProductCode= request.POST.get("ProductCode")
@@ -43,14 +59,14 @@ def Product_Info(request):
         InventoryAC=request.POST.get("InventoryAC")
         SALE_AC=request.POST.get("SALE_AC") 
         COG_AC=request.POST.get("COG_AC")
-        IMAGE=request.POST.get('IMAGE')
+        IMAGE=request.FILES['IMAGE']
         Trade_T= request.POST.get("Trade_T")
         Project_T=request.POST.get("Project_T")
         Branch_T=request.POST.get("Branch_T")
         LocCode=request.POST.get("LocCode")
         innerUnit =request.POST.get("innerUnit")
     
-       
+        print(IMAGE)
         if Scheme is None :
             Scheme = 'Null'
         if OIUnit == '':
